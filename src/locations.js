5d141d18df55dedeb99aa0c2c4372731802bfa0f
theyousry <theyousry@gmail.com>
2019-01-03 09:22:13 +0200

    Locations Added

---
+++ src/locations.js
@@ -0,0 +1,50 @@
+export  const locations = [
+    {
+        title: "Cairo Tower",
+        location: {
+            lat: 30.046370,
+            lng: 31.224340
+        },
+        venueId: "4c3c3e06a9509c74150c395b"
+    },
+    {
+        title: "The Nile River",
+        location: {
+            lat: 30.04599026629391,
+            lng: 31.229968070983883
+        },
+        venueId: "4dc530dcfa76d685cdd93f94"
+    },
+    {
+        title: "The Egyptian Museum",
+        location: {
+            lat: 30.047902,
+            lng: 31.233564
+        },
+        venueId: "4b653727f964a5203ee92ae3"
+    },
+    {
+        title: "The Nile Ritz-Carlton",
+        location: {
+            lat: 30.0459653012245,
+            lng: 31.232135764875625
+        },
+        venueId: "50159fd8e4b07f20b3e58d35"
+    },
+    {
+        title: "Tahrir Square",
+        location: {
+            lat: 30.044420727931577,
+            lng: 31.235707998275757
+        },
+        venueId: "4cb48a7e1463a143dfb7bba9"
+    },
+    {
+        title: "Cairo Opera House",
+        location: {
+            lat: 30.042497,
+            lng: 31.224415
+        },
+        venueId: "4c963c5303413704fd8982ef"
+    }
+]
